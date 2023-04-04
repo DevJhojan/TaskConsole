8a63462d599770adb526859c0dafea84c836dd3b
jdtp125753 <jhojan1806@gmail.com>
2023-04-04 17:37:22 -0500

    update tiltle

--- Components/Stard/StardingTitle.cs
+++ Components/Stard/StardingTitle.cs
@@ -5,16 +5,16 @@ namespace TaskConsole.Components.Stard
         public void Title()
         {
             Console.ForegroundColor=ConsoleColor.DarkMagenta;
-            Console.WriteLine("--------------------------------------------------------------------------------------------");
-            Console.WriteLine("---TTTTTTTTTTT-----AAAAAA-----KK----KK-----SSSSSS------------ccccc------oooo----NNNN-----NN-----SSSSSS------oooo---");
-            Console.WriteLine("-------TT---------AA----AA----KK---KK-----SS----SS---------cc----cc----oo--oo---NN-NN----NN----SS----SS----oo--oo-");
-            Console.WriteLine("-------TT--------AA------AA---KK--KK-----SS----------------cc---------oo----oo--NN--NN---NN---SS----------oo----oo--");
-            Console.WriteLine("-------TT--------AAAAAAAAAA---KKkKK-------SSSSSSS----------cc---------oo----oo--NN---NN--NN----SSSSSSS-------");
-            Console.WriteLine("-------TT--------AA------AA---KK--KK------------SS---------cc---------oo----oo--NN----NN-NN----------SS-------");
-            Console.WriteLine("-------TT--------AA------AA---KK---KK------------SS--------cc---------oo----oo--NN-----NNNN-----------SS-----");
-            Console.WriteLine("-------TT--------AA------AA---KK----KK-----SS----SS---------cc---cc----oo--oo---NN------NNN-----SS----SS-----");
-            Console.WriteLine("-------TT--------AA------AA---KK-----KK-----SSSSSS-----------ccccc------oooo----NN-------NN------SSSSSS--------");
-            Console.WriteLine("--------------------------------------------------------------------------------------------");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------");
+            Console.WriteLine("---TTTTTTTTTTT-----AAAAAA-----KK----KK-----SSSSSS------------CCCCC------OOOO----NNNN-----NN-----SSSSSS------OOOO----LL-------EEEEEE-");
+            Console.WriteLine("-------TT---------AA----AA----KK---KK-----SS----SS---------CC----CC----OO--OO---NN-NN----NN----SS----SS----OO--OO---LL-------EE-----");
+            Console.WriteLine("-------TT--------AA------AA---KK--KK------SS---------------CC---------OO----OO--NN--NN---NN----SS---------OO----OO--LL-------EE-----");
+            Console.WriteLine("-------TT--------AAAAAAAAAA---KKkKK-------SSSSSSS----------CC---------OO----OO--NN---NN--NN----SSSSSSS----OO----OO--LL-------EEEEEE-");
+            Console.WriteLine("-------TT--------AA------AA---KK--KK------------SS---------CC---------OO----OO--NN----NN-NN----------SS---OO----OO--LL-------EE-----");
+            Console.WriteLine("-------TT--------AA------AA---KK---KK------------SS--------CC---------OO----OO--NN-----NNNN-----------SS--OO----OO--LL-------EE-----");
+            Console.WriteLine("-------TT--------AA------AA---KK----KK-----SS----SS---------CC---CC----OO--OO---NN------NNN-----SS----SS---OO--OO---LL-------EE-----");
+            Console.WriteLine("-------TT--------AA------AA---KK-----KK-----SSSSSS-----------CCCCC------OOOO----NN-------NN------SSSSSS-----OOOO----LLLLLL---EEEEEE-");
+            Console.WriteLine("------------------------------------------------------------------------------------------------------------------------------------");
             Console.ForegroundColor = ConsoleColor.DarkYellow;
         }
     }
